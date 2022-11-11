29e8881063260c4a7ae2571379fe25849cb4a046
QuiverDance <pjw7200@ajou.ac.kr>
2022-11-11 23:30:17 +0900

    refactor: added properties

--- src/main/java/com/ajoufarmers/farmer/usermangement/login/dto/UserDto.java
+++ src/main/java/com/ajoufarmers/farmer/usermangement/login/dto/UserDto.java
@@ -1,4 +1,13 @@
 package com.ajoufarmers.farmer.usermangement.login.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class UserDto {
+    private Long id;
+    private String email;
 }
