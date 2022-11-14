db3c64ebb14ccec37ac00c32e13ddf6b1051d357
QuiverDance <pjw7200@ajou.ac.kr>
2022-11-14 23:22:04 +0900

    refactor: temporary filter setting

--- src/main/java/com/ajoufarmers/farmer/config/SecurityConfig.java
+++ src/main/java/com/ajoufarmers/farmer/config/SecurityConfig.java
@@ -39,26 +39,39 @@ public class SecurityConfig {
     @Bean
     @Order(SecurityProperties.BASIC_AUTH_ORDER)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().ignoringAntMatchers("/api/**") /* REST API 사용 예외처리 */
+//        http
+//                .csrf().ignoringAntMatchers("/api/**") /* REST API 사용 예외처리 */
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers("/", "/auth/**", "/posts/read/**", "/posts/search/**").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+////                .formLogin()
+////                .loginPage("/login")
+////                //.loginProcessingUrl("/auth/loginProc")
+////                .defaultSuccessUrl("/")
+////                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .invalidateHttpSession(true).deleteCookies("JSESSIONID")
+//                .logoutSuccessUrl("/")
+//                .and() /* OAuth */
+//                .oauth2Login()
+//                .userInfoEndpoint() // OAuth2 로그인 성공 후 가져올 설정들
+//                .userService(customOAuth2UserService); // 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
+        http.csrf().disable()
+                .headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/auth/**", "/posts/read/**", "/posts/search/**").permitAll()
+                .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**","/profile").permitAll()
+                .antMatchers("/api/v1/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/auth/login")
-                .loginProcessingUrl("/auth/loginProc")
-                .defaultSuccessUrl("/")
+                .logout().logoutSuccessUrl("/")
                 .and()
-                .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .invalidateHttpSession(true).deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/")
-                .and() /* OAuth */
                 .oauth2Login()
-                .userInfoEndpoint() // OAuth2 로그인 성공 후 가져올 설정들
-                .userService(customOAuth2UserService); // 서버에서 사용자 정보를 가져온 상태에서 추가로 진행하고자 하는 기능 명시
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
 
         return http.build();
     }
