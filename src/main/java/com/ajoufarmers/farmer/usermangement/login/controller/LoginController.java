ff20db4abefa47d799e281690310cde696a1c01a
QuiverDance <pjw7200@ajou.ac.kr>
2022-11-14 23:33:18 +0900

    added: LoginController

---
+++ src/main/java/com/ajoufarmers/farmer/usermangement/login/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.ajoufarmers.farmer.usermangement.login.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login/oauth2/code/google")
+    public String redirection(){
+        return "Hello World!"; //Redirect url 반환
+    }
+}
