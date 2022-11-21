fc0fa89e7eff08fbe6c01434af1599c485fb98cf
QuiverDance <pjw7200@ajou.ac.kr>
2022-11-22 00:00:22 +0900

    refactor: added PlantService

--- src/main/java/com/ajoufarmers/farmer/mypage/controller/MyPageController.java
+++ src/main/java/com/ajoufarmers/farmer/mypage/controller/MyPageController.java
@@ -2,6 +2,8 @@ package com.ajoufarmers.farmer.mypage.controller;
 
 import com.ajoufarmers.farmer.mypage.entity.MyPageEntry;
 import com.ajoufarmers.farmer.mypage.service.MyPageEntryService;
+import com.ajoufarmers.farmer.plantdictionary.entity.Plant;
+import com.ajoufarmers.farmer.plantdictionary.service.PlantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +13,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class MyPageController {
     private final MyPageEntryService myPageEntryService;
+    private final PlantService plantService;
 
     @Autowired
-    public MyPageController(MyPageEntryService myPageEntryService){
+    public MyPageController(MyPageEntryService myPageEntryService, PlantService plantService){
         this.myPageEntryService = myPageEntryService;
+        this.plantService = plantService;
     }
 
     @PostMapping("/mypage/new")
@@ -63,5 +73,23 @@ public class MyPageController {
             newEntry.setWaterDate(waterDate);
             myPageEntryService.updateMyPlant(newEntry);
         }
+    }
+    @GetMapping("/mypage/watertiming")
+    public ResponseEntity<?> getWaterTimingCheckList() throws ParseException {
+        List<MyPageEntry> myPageEntryList = myPageEntryService.findMyPlants();
+        List<Boolean> checkList = new ArrayList<>(myPageEntryList.size());
+
+        for(int i = 0; i < myPageEntryList.size(); i++){
+            Date lateDate = new SimpleDateFormat("yyyy-MM-dd").parse(myPageEntryList.get(i).getWaterDate());
+            Date curDate = new SimpleDateFormat("yyyy-MM-dd").parse(LocalDate.now().toString());
+            long diffSec = (curDate.getTime() - lateDate.getTime())/1000;
+
+            Plant plantInfo = plantService.findOne(myPageEntryList.get(i).getPlantId()).get();
+            if(diffSec/(24*60*60) >= plantInfo.getWaterDate())
+                checkList.set(i, true);
+            else
+                checkList.set(i, false);
+        }
+        return new ResponseEntity<>(checkList, HttpStatus.OK);
     }
 }
