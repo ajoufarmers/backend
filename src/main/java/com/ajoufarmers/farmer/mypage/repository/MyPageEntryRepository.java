15f5bf6c6934e7a720906aa5d3cd8a9ab46d4e55
QuiverDance <pjw7200@ajou.ac.kr>
2022-11-21 23:22:20 +0900

    refactor: remove update function

--- src/main/java/com/ajoufarmers/farmer/mypage/repository/MyPageEntryRepository.java
+++ src/main/java/com/ajoufarmers/farmer/mypage/repository/MyPageEntryRepository.java
@@ -2,15 +2,13 @@ package com.ajoufarmers.farmer.mypage.repository;
 
 import com.ajoufarmers.farmer.mypage.entity.MyPageEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface MyPageEntryRepository extends JpaRepository<MyPageEntry, Long> {
     MyPageEntry save(MyPageEntry myPageEntry);
-
-    Optional<MyPageEntry> update(MyPageEntry myPageEntry);
-
     List<MyPageEntry> findAll();
     Optional<MyPageEntry> findById(Long id);
 }
