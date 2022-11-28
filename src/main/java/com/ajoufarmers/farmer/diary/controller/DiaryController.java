31d9d4275bf69ce4ab935c0894a225d1b488bc04
지수 이 <itis821@ajou.ac.kr>
2022-11-29 05:06:14 +0900

    ADD : DiaryController -> getDiaryList()

--- src/main/java/com/ajoufarmers/farmer/diary/controller/DiaryController.java
+++ src/main/java/com/ajoufarmers/farmer/diary/controller/DiaryController.java
@@ -1,10 +1,14 @@
 package com.ajoufarmers.farmer.diary.controller;
 
+import com.ajoufarmers.farmer.diary.dto.DiaryDto;
 import com.ajoufarmers.farmer.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,6 +17,10 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    @GetMapping("/list/{memberId}")
+    public List<DiaryDto> getDiaryList(@PathVariable Long memberId){
+        return diaryService.getDiaryListByMemberId(memberId);
+    }
 
 
 }
